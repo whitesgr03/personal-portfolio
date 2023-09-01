27d07080459572a8ce0b810609e4dcec200cad32
Weiss Bai <whitesgr03@gmail.com>
2023-09-01 19:19:24 +0800

    Add handleChangeTheme

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,9 @@ const App = () => {
 	const [darkTheme, setDarkTheme] = useState(
 		window.matchMedia("(prefers-color-scheme: dark)")
 	);
+
+	const handleChangeTheme = () => setDarkTheme(!darkTheme);
+
 	return (
 		<div className="app">
 		</div>
