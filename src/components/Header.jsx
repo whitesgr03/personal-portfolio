9ebc4a7496e5d1b628fb66e589ba487f3053162b
Weiss Bai <whitesgr03@gmail.com>
2023-09-01 19:18:00 +0800

    Add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+import Icon from "@mdi/react";
+import {
+	mdiWeatherSunny,
+	mdiWeatherNight,
+	mdiClipboardListOutline,
+	mdiAccountOutline,
+} from "@mdi/js";
+
+const Header = ({ darkTheme, onChangeTheme }) => {
+	return (
+		<header>
+			<nav>
+				<h1>
+					<a href="#home">
+						Weiss <br /> Bai
+					</a>
+				</h1>
+				<ul>
+					<li>
+						<a href="#about">
+							<Icon path={mdiAccountOutline} />
+							<span>About</span>
+						</a>
+					</li>
+					<li>
+						<a href="#products">
+							<Icon path={mdiClipboardListOutline} />
+							<span>Projects</span>
+						</a>
+					</li>
+					<li>
+						<button
+							type="button"
+							aria-label="theme"
+							onClick={onChangeTheme}
+						>
+							{darkTheme ? (
+								<Icon path={mdiWeatherNight} />
+							) : (
+								<Icon path={mdiWeatherSunny} />
+							)}
+							<span>Theme</span>
+						</button>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
