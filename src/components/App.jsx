2eb0c270559eb09d2f5c45de314aef06125f2b18
Weiss Bai <whitesgr03@gmail.com>
2023-09-01 19:19:13 +0800

    Add useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
+
 import Header from "./Header";
 const App = () => {
+	const [darkTheme, setDarkTheme] = useState(
+		window.matchMedia("(prefers-color-scheme: dark)")
+	);
 	return (
 		<div className="app">
 		</div>
