89ccdded799186c4ce76b4a3c807ad0588a75b15
Weiss Bai <whitesgr03@gmail.com>
2023-09-02 19:47:23 +0800

    Create Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import Icon from "@mdi/react";
+import { mdiEmailOutline, mdiGithub } from "@mdi/js";
+
+const Footer = () => {
+	return (
+		<footer>
+			<div className="contact">
+				<h3>Contact me</h3>
+				<p>
+					Please get in touch if you think our work could be mutually
+					beneficial!
+				</p>
+				<address>
+					<Icon path={mdiEmailOutline} />
+					<p>whitesgr03@gmail.com</p>
+				</address>
+			</div>
+			<div className="copyright">
+				<a href="#gitHub">
+					<Icon path={mdiGithub} />
+				</a>
+				<p>&copy; 2023 - Designed & coded by Weiss Bai</p>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
