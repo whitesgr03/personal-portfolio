b8a89bf8222c3b6d8ca2811ec631e5e9c63675c5
Weiss Bai <whitesgr03@gmail.com>
2023-09-05 14:26:36 +0800

    Create Products component

---
+++ src/components/Products.jsx
@@ -0,0 +1,76 @@
+import openNewTabIcon from "../image/open-in-new-icon.png";
+import githubIcon from "../image/github-icon.png";
+
+const Products = () => {
+	return (
+		<div id="products" className="products">
+			<h2>My Works</h2>
+
+			<ul>
+				<li>
+					<img src="https://fakeimg.pl/350x200/" alt="product1" />
+					<article>
+						<h3>product1</h3>
+
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing
+							elit. Perspiciatis reprehenderit adipisci tenetur,
+							rerum impedit ex atque fugiat repudiandae ad ipsum
+							temporibus totam! Repudiandae iure nisi quidem unde
+							laudantium, totam nobis!
+						</p>
+
+						<div class="link">
+							<a href="#live" target="_blank">
+								Live
+								<img
+									src={openNewTabIcon}
+									alt="live (opens in new tab)"
+								/>
+							</a>
+							<a href="#source" target="_blank">
+								Source
+								<img
+									src={githubIcon}
+									alt="github (opens in new tab)"
+								/>
+							</a>
+						</div>
+					</article>
+				</li>
+				<li>
+					<img src="https://fakeimg.pl/350x200/" alt="product2" />
+					<article>
+						<h3>product2</h3>
+
+						<p>
+							Lorem ipsum dolor, sit amet consectetur adipisicing
+							elit. Quisquam optio quis dicta laborum
+							reprehenderit iste asperiores doloremque labore
+							molestiae delectus?
+						</p>
+
+						<div class="link">
+							<a href="#live" target="_blank">
+								Live
+								<img
+									src={openNewTabIcon}
+									alt="live (opens in new tab)"
+								/>
+							</a>
+							<a href="#source" target="_blank">
+								Source
+								<img
+									src={githubIcon}
+									alt="github (opens in new tab)"
+								/>
+							</a>
+						</div>
+					</article>
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Products;
