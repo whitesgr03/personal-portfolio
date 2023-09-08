0cd1a0f5cc44c1daff99a758c787051328ac2ec4
Weiss Bai <whitesgr03@gmail.com>
2023-09-08 12:42:22 +0800

    Remove contact block

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,13 @@
 import Icon from "@mdi/react";
-import { mdiEmailOutline, mdiGithub } from "@mdi/js";
+import { mdiGithub } from "@mdi/js";
 
 const Footer = () => {
 	return (
 		<footer>
-			<div className="contact">
-				<h2>Contact me</h2>
-				<p>
-					Please get in touch if you think our work could be mutually
-					beneficial!
-				</p>
-				<address>
-					<Icon path={mdiEmailOutline} />
-					<p>whitesgr03@gmail.com</p>
-				</address>
-			</div>
-			<div className="copyright">
-				<a href="#gitHub">
-					<Icon path={mdiGithub} />
-				</a>
-				<p>&copy; 2023 - Designed & coded by Weiss Bai</p>
-			</div>
+			<a href="#gitHub">
+				<Icon path={mdiGithub} />
+			</a>
+			<p>&copy; 2023 - Designed & coded by Weiss Bai</p>
 		</footer>
 	);
 };
