13eb42104f2b526c995602e6d066ce9b808e5623
Weiss Bai <whitesgr03@gmail.com>
2023-09-15 13:51:55 +0800

    Create Modal Component

---
+++ src/components/Modal.jsx
@@ -0,0 +1,43 @@
+const Modal = ({ showProduct, onCloseModal }) => {
+	return (
+		<div className={`modal ${showProduct ? "show" : ""}`}>
+			<ProductModal onCloseModal={onCloseModal} />
+		</div>
+	);
+};
+
+const ProductModal = ({ onCloseModal }) => (
+	<div className="productModal">
+		<button type="button" className="close" onClick={onCloseModal}></button>
+		<div className="describe">
+			<h3>Product 1 Product 1 Product 1</h3>
+			<div>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi,
+				voluptate.Lorem ipsum dolor sit amet consectetur, adipisicing
+				elit. Modi, voluptate.Lorem ipsum dolor sit amet consectetur,
+				adipisicing elit. Modi, voluptate.Lorem ipsum dolor sit amet
+				consectetur, adipisicing elit. Modi, voluptate.Lorem ipsum dolor
+				sit amet consectetur, adipisicing elit. Modi, voluptate.
+			</div>
+		</div>
+		<ul>
+			<li>
+				<img
+					sizes="(max-width: 1920px) 100vw, 1920px"
+					srcSet={`https://fakeimg.pl/480x320/195c86/ 480w, https://fakeimg.pl/1004x669/195c86/ 1004w,https://fakeimg.pl/1372x915/195c86/ 1372w,https://fakeimg.pl/1820x1213/195c86/ 1820w`}
+					src="https://fakeimg.pl/1920x1280/195c86/"
+					alt="product1"
+				/>
+			</li>
+			<li>
+				<img
+					sizes="(max-width: 1920px) 100vw, 1920px"
+					srcSet={`https://fakeimg.pl/480x320/195c86/ 480w, https://fakeimg.pl/1004x669/195c86/ 1004w,https://fakeimg.pl/1372x915/195c86/ 1372w,https://fakeimg.pl/1820x1213/195c86/ 1820w`}
+					src="https://fakeimg.pl/1920x1280/195c86/"
+					alt="product1"
+				/>
+			</li>
+		</ul>
+	</div>
+);
+export default Modal;
