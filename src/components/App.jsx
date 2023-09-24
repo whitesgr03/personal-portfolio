1ec874662d7c3d2b48655b36bc1331742198c1cb
Weiss Bai <whitesgr03@gmail.com>
2023-09-24 17:57:57 +0800

    Change prop name to onCloseModal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -39,7 +39,7 @@ const App = () => {
 		>
 			<Modal
 				showProduct={showProduct}
-				onIfShowProduct={handleIfShowProduct}
+				onCloseModal={handleIfShowProduct}
 			/>
 			<Header
 				aboutRef={aboutRef}
