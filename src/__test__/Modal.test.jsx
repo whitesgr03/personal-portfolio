dd9917eeef8632a5ff791f5a3ef927c055d7e942
Weiss Bai <whitesgr03@gmail.com>
2023-09-25 13:57:22 +0800

    Remove test

--- src/__test__/Modal.test.jsx
+++ src/__test__/Modal.test.jsx
@@ -1,5 +1,4 @@
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 
 import Modal, { ProductModal } from "../components/Modal";
 
@@ -20,72 +19,6 @@ describe("Renders Modal Component", () => {
 
 		expect(element).not.toHaveClass("show");
 	});
-	it("should close modal DOM with click", async () => {
-		const mockCloseModal = jest.fn();
-		const user = userEvent.setup();
-
-		render(<Modal onCloseModal={mockCloseModal} />);
-
-		const button = screen.getByRole("button");
-
-		await user.pointer({ keys: "[MouseLeft]", target: button });
-
-		expect(mockCloseModal).toBeCalledTimes(1);
-	});
-	// it("should disable scroll event when pointer over modal block", async () => {
-	// 	const mockShowProduct = true;
-	// 	const user = userEvent.setup();
-
-	// 	render(<Modal showProduct={mockShowProduct} />);
-
-	// const paddingBlock = screen.getByTestId("padding");
-	// const modalBlock = screen.getByTestId("modal");
-
-	// 	await waitFor(() => {
-	// 		user.pointer([
-	// 			{ keys: "[TouchA]", target: modalBlock },
-	// 			{ pointerName: "TouchA", target: paddingBlock },
-	// 			{ keys: "[/TouchA]" },
-	// 		]);
-
-	// 		expect(modalBlock).toHaveClass("modal", "show", "unScroll");
-	// 	});
-	// });
-	// it("should trigger scroll event when pointer out modal block", async () => {
-	// 	const mockShowProduct = true;
-	// 	const user = userEvent.setup();
-
-	// 	render(<Modal showProduct={mockShowProduct} />);
-
-	// 	const modalBlock = screen.getByTestId("modal");
-	// 	const paddingBlock = screen.getByTestId("padding");
-
-	// 	await waitFor(() => {
-	// 		user.pointer([
-	// 			{ keys: "[TouchA]", target: modalBlock },
-	// 			{ pointerName: "TouchA", target: paddingBlock },
-	// 			{ pointerName: "TouchA", target: modalBlock },
-	// 			{ keys: "[/TouchA]" },
-	// 		]);
-
-	// 		expect(modalBlock).not.toHaveClass("unScroll");
-	// 	});
-	// });
-	// it("should change modal block background color with scroll", async () => {
-	// 	const mockShowProduct = true;
-	// 	render(<Modal showProduct={mockShowProduct} />);
-
-	// 	// const modalBlock = screen.getByTestId("modal");
-	// 	const paddingBlock = screen.getByTestId("padding");
-
-	// 	// paddingBlock.height = 500;
-
-	// 	// console.log(getComputedStyle(paddingBlock));
-	// 	// await fireEvent.scroll(modalBlock, { target: { scrollTop: 400 } });
-
-	// 	expect(paddingBlock).toHaveStyle("height: 200px");
-	// 	// console.log(modalBlock.className);
-	// });
 });
 
 describe("Renders ProductModal Component", () => {
