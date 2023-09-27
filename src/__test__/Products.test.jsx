e46a769ecea5d4bfeb49cb3c4ae15b16c51a984e
Weiss Bai <whitesgr03@gmail.com>
2023-09-27 12:45:35 +0800

    Add test should disable button from being focused

--- src/__test__/Products.test.jsx
+++ src/__test__/Products.test.jsx
@@ -32,4 +32,27 @@ describe("Renders Products Component", () => {
 
 		expect(mockShowProduct).toBeCalledTimes(2);
 	});
+	it("Should disable button from being focused", () => {
+		const mokeShowProduct = true;
+
+		render(<Products showProduct={mokeShowProduct} />);
+
+		const [firstButtonForImage] = screen.getAllByRole("button", {
+			name: "buttonForImage",
+		});
+		const [firstButtonForTitle] = screen.getAllByRole("button", {
+			name: "buttonForTitle",
+		});
+		const [firstLinkForLive] = screen.getAllByRole("link", {
+			name: "Live (opens in new tab)",
+		});
+		const [firstLinkForSource] = screen.getAllByRole("link", {
+			name: "Source (opens in new tab)",
+		});
+
+		expect(firstButtonForImage).toHaveAttribute("tabIndex", "-1");
+		expect(firstButtonForTitle).toHaveAttribute("tabIndex", "-1");
+		expect(firstLinkForLive).toHaveAttribute("tabIndex", "-1");
+		expect(firstLinkForSource).toHaveAttribute("tabIndex", "-1");
+	});
 });
