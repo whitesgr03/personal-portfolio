c0db7875eca0671cccbf6753aeb9b1b515d1a670
Weiss Bai <whitesgr03@gmail.com>
2023-09-28 17:41:33 +0800

    Add mockAllProducts

--- src/__test__/Products.test.jsx
+++ src/__test__/Products.test.jsx
@@ -103,6 +103,53 @@ describe("Renders Products Component", () => {
 
 	it("should display the modal with click", async () => {
 		const mockShowProduct = jest.fn();
+
+		const mockAllProducts = [
+			{
+				id: "project1",
+				name: "project1",
+				image: {
+					preview: {
+						url: {
+							phone: "https://fakeimg.pl/480x320/195c86/",
+							table: "https://fakeimg.pl/1004x669/195c86/",
+							laptop: "https://fakeimg.pl/1372x915/195c86/",
+							desktop: "https://fakeimg.pl/1820x1213/195c86/",
+						},
+					},
+					pictures: [
+						{
+							id: "project1-picture1",
+							url: {
+								phone: "https://fakeimg.pl/480x320/195c86/",
+								table: "https://fakeimg.pl/1004x669/195c86/",
+								laptop: "https://fakeimg.pl/1372x915/195c86/",
+								desktop: "https://fakeimg.pl/1820x1213/195c86/",
+							},
+						},
+						{
+							id: "project1-picture2",
+							url: {
+								phone: "https://fakeimg.pl/480x320/195c86/",
+								table: "https://fakeimg.pl/1004x669/195c86/",
+								laptop: "https://fakeimg.pl/1372x915/195c86/",
+								desktop: "https://fakeimg.pl/1820x1213/195c86/",
+							},
+						},
+						{
+							id: "project1-picture3",
+							url: {
+								phone: "https://fakeimg.pl/480x320/195c86/",
+								table: "https://fakeimg.pl/1004x669/195c86/",
+								laptop: "https://fakeimg.pl/1372x915/195c86/",
+								desktop: "https://fakeimg.pl/1820x1213/195c86/",
+							},
+						},
+					],
+				},
+			},
+		];
+
 		const user = userEvent.setup();
 
 		render(<Products onShowProduct={mockShowProduct} />);
