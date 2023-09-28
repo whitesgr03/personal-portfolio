a4c6f033045e0c488d1efab4a64e2390d3ade953
Weiss Bai <whitesgr03@gmail.com>
2023-09-28 17:40:20 +0800

    Pass mokeProduct to render ProductModal

--- src/__test__/Modal.test.jsx
+++ src/__test__/Modal.test.jsx
@@ -23,6 +23,51 @@ describe("Renders Modal Component", () => {
 
 describe("Renders ProductModal Component", () => {
 	it("Should return ProductModal DOM", () => {
+		const mokeProduct = {
+			id: "project1",
+			name: "project1",
+			image: {
+				preview: {
+					url: {
+						phone: "https://fakeimg.pl/480x320/195c86/",
+						table: "https://fakeimg.pl/1004x669/195c86/",
+						laptop: "https://fakeimg.pl/1372x915/195c86/",
+						desktop: "https://fakeimg.pl/1820x1213/195c86/",
+					},
+				},
+				pictures: [
+					{
+						id: "project1-picture1",
+						url: {
+							phone: "https://fakeimg.pl/480x320/195c86/",
+							table: "https://fakeimg.pl/1004x669/195c86/",
+							laptop: "https://fakeimg.pl/1372x915/195c86/",
+							desktop: "https://fakeimg.pl/1820x1213/195c86/",
+						},
+					},
+					{
+						id: "project1-picture2",
+						url: {
+							phone: "https://fakeimg.pl/480x320/195c86/",
+							table: "https://fakeimg.pl/1004x669/195c86/",
+							laptop: "https://fakeimg.pl/1372x915/195c86/",
+							desktop: "https://fakeimg.pl/1820x1213/195c86/",
+						},
+					},
+					{
+						id: "project1-picture3",
+						url: {
+							phone: "https://fakeimg.pl/480x320/195c86/",
+							table: "https://fakeimg.pl/1004x669/195c86/",
+							laptop: "https://fakeimg.pl/1372x915/195c86/",
+							desktop: "https://fakeimg.pl/1820x1213/195c86/",
+						},
+					},
+				],
+			},
+		};
+
+		const { container } = render(<ProductModal product={mokeProduct} />);
 
 		expect(container).toMatchSnapshot();
 	});
