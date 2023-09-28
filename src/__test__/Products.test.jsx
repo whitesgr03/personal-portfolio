36615d9c7e5e05e43cd66bf69a17effb6d5c0f3f
Weiss Bai <whitesgr03@gmail.com>
2023-09-28 17:40:43 +0800

    Add mockAllProducts

--- src/__test__/Products.test.jsx
+++ src/__test__/Products.test.jsx
@@ -5,7 +5,95 @@ import Products from "../components/Products";
 
 describe("Renders Products Component", () => {
 	it("should return Products DOM", () => {
-		const { container } = render(<Products />);
+		const mockAllProducts = [
+			{
+				id: "project1",
+				name: "project1",
+				image: {
+					preview: {
+						url: {
+							phone: "https://fakeimg.pl/480x320/195c86/",
+							table: "https://fakeimg.pl/1004x669/195c86/",
+							laptop: "https://fakeimg.pl/1372x915/195c86/",
+							desktop: "https://fakeimg.pl/1820x1213/195c86/",
+						},
+					},
+					pictures: [
+						{
+							id: "project1-picture1",
+							url: {
+								phone: "https://fakeimg.pl/480x320/195c86/",
+								table: "https://fakeimg.pl/1004x669/195c86/",
+								laptop: "https://fakeimg.pl/1372x915/195c86/",
+								desktop: "https://fakeimg.pl/1820x1213/195c86/",
+							},
+						},
+						{
+							id: "project1-picture2",
+							url: {
+								phone: "https://fakeimg.pl/480x320/195c86/",
+								table: "https://fakeimg.pl/1004x669/195c86/",
+								laptop: "https://fakeimg.pl/1372x915/195c86/",
+								desktop: "https://fakeimg.pl/1820x1213/195c86/",
+							},
+						},
+						{
+							id: "project1-picture3",
+							url: {
+								phone: "https://fakeimg.pl/480x320/195c86/",
+								table: "https://fakeimg.pl/1004x669/195c86/",
+								laptop: "https://fakeimg.pl/1372x915/195c86/",
+								desktop: "https://fakeimg.pl/1820x1213/195c86/",
+							},
+						},
+					],
+				},
+			},
+			{
+				id: "project2",
+				name: "project2",
+				image: {
+					preview: {
+						url: {
+							phone: "https://fakeimg.pl/480x320/195c86/",
+							table: "https://fakeimg.pl/1004x669/195c86/",
+							laptop: "https://fakeimg.pl/1372x915/195c86/",
+							desktop: "https://fakeimg.pl/1820x1213/195c86/",
+						},
+					},
+					pictures: [
+						{
+							id: "project2-picture1",
+							url: {
+								phone: "https://fakeimg.pl/480x320/195c86/",
+								table: "https://fakeimg.pl/1004x669/195c86/",
+								laptop: "https://fakeimg.pl/1372x915/195c86/",
+								desktop: "https://fakeimg.pl/1820x1213/195c86/",
+							},
+						},
+						{
+							id: "project2-picture2",
+							url: {
+								phone: "https://fakeimg.pl/480x320/195c86/",
+								table: "https://fakeimg.pl/1004x669/195c86/",
+								laptop: "https://fakeimg.pl/1372x915/195c86/",
+								desktop: "https://fakeimg.pl/1820x1213/195c86/",
+							},
+						},
+						{
+							id: "project2-picture3",
+							url: {
+								phone: "https://fakeimg.pl/480x320/195c86/",
+								table: "https://fakeimg.pl/1004x669/195c86/",
+								laptop: "https://fakeimg.pl/1372x915/195c86/",
+								desktop: "https://fakeimg.pl/1820x1213/195c86/",
+							},
+						},
+					],
+				},
+			},
+		];
+
 
 		expect(container).toMatchSnapshot();
 	});
