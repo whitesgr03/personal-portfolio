65bb6726412d4d802e671267e8ddd85f71285088
Weiss Bai <whitesgr03@gmail.com>
2023-09-28 17:31:30 +0800

    Move condition to ProductModal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -33,39 +33,41 @@ const Modal = forwardRef(({ showModal, product }, ref) => {
 			>
 				<button type="button" className="close"></button>
 			</div>
-			{product && <ProductModal product={product} />}
+			<ProductModal product={product} />
 		</div>
 	);
 });
 
 const ProductModal = ({ product }) => {
 	return (
-		<div className="productModal">
-			<div className="describe">
-				<h3>{product.name}</h3>
-				<div>
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-					Modi, voluptate.Lorem ipsum dolor sit amet consectetur,
-					adipisicing elit. Modi, voluptate.Lorem ipsum dolor sit amet
-					consectetur, adipisicing elit. Modi, voluptate.Lorem ipsum
-					dolor sit amet consectetur, adipisicing elit. Modi,
-					voluptate.Lorem ipsum dolor sit amet consectetur,
-					adipisicing elit. Modi, voluptate.
+		product && (
+			<div className="productModal">
+				<div className="describe">
+					<h3>{product.name}</h3>
+					<div>
+						Lorem ipsum dolor sit amet consectetur, adipisicing
+						elit. Modi, voluptate.Lorem ipsum dolor sit amet
+						consectetur, adipisicing elit. Modi, voluptate.Lorem
+						ipsum dolor sit amet consectetur, adipisicing elit.
+						Modi, voluptate.Lorem ipsum dolor sit amet consectetur,
+						adipisicing elit. Modi, voluptate.Lorem ipsum dolor sit
+						amet consectetur, adipisicing elit. Modi, voluptate.
+					</div>
 				</div>
+				<ul>
+					{product.image.pictures.map(picture => (
+						<li key={picture.id}>
+							<img
+								sizes="(max-width: 1920px) 100vw, 1920px"
+								srcSet={`${picture.url.phone} 480w, ${picture.url.table} 1004w, ${picture.url.laptop} 1372w, ${picture.url.desktop} 1820w`}
+								src={picture.url.desktop}
+								alt={product.name}
+							/>
+						</li>
+					))}
+				</ul>
 			</div>
-			<ul>
-				{product.image.pictures.map(picture => (
-					<li key={picture.id}>
-						<img
-							sizes="(max-width: 1920px) 100vw, 1920px"
-							srcSet={`${picture.url.phone} 480w, ${picture.url.table} 1004w, ${picture.url.laptop} 1372w, ${picture.url.desktop} 1820w`}
-							src={picture.url.desktop}
-							alt={product.name}
-						/>
-					</li>
-				))}
-			</ul>
-		</div>
+		)
 	);
 };
 
