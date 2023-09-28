218d15ada04d064e85ae1d998eee2b95832c751f
Weiss Bai <whitesgr03@gmail.com>
2023-09-28 17:43:31 +0800

    Add mockAllProducts

--- src/__test__/Products.test.jsx
+++ src/__test__/Products.test.jsx
@@ -177,6 +177,52 @@ describe("Renders Products Component", () => {
 	});
 	it("Should disable focus button", () => {
 		const mokeShowModal = true;
+		const mockAllProducts = [
+			{
+				id: "project1",
+				name: "project1",
+				image: {
+					preview: {
+						url: {
+							phone: "https://fakeimg.pl/480x320/195c86/",
+							table: "https://fakeimg.pl/1004x669/195c86/",
+							laptop: "https://fakeimg.pl/1372x915/195c86/",
+							desktop: "https://fakeimg.pl/1820x1213/195c86/",
+						},
+					},
+					pictures: [
+						{
+							id: "project1-picture1",
+							url: {
+								phone: "https://fakeimg.pl/480x320/195c86/",
+								table: "https://fakeimg.pl/1004x669/195c86/",
+								laptop: "https://fakeimg.pl/1372x915/195c86/",
+								desktop: "https://fakeimg.pl/1820x1213/195c86/",
+							},
+						},
+						{
+							id: "project1-picture2",
+							url: {
+								phone: "https://fakeimg.pl/480x320/195c86/",
+								table: "https://fakeimg.pl/1004x669/195c86/",
+								laptop: "https://fakeimg.pl/1372x915/195c86/",
+								desktop: "https://fakeimg.pl/1820x1213/195c86/",
+							},
+						},
+						{
+							id: "project1-picture3",
+							url: {
+								phone: "https://fakeimg.pl/480x320/195c86/",
+								table: "https://fakeimg.pl/1004x669/195c86/",
+								laptop: "https://fakeimg.pl/1372x915/195c86/",
+								desktop: "https://fakeimg.pl/1820x1213/195c86/",
+							},
+						},
+					],
+				},
+			},
+		];
+
 
 		const [firstButtonForImage] = screen.getAllByRole("button", {
 			name: "buttonForImage",
