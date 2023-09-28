82bf2122f5212c93470b036f12b72237d14bb4ba
Weiss Bai <whitesgr03@gmail.com>
2023-09-28 16:04:12 +0800

    Remove if conditional expression

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -47,11 +47,6 @@ const App = () => {
 	useEffect(() => {
 		(async () => {
 			const fakeResult = await getProducts();
-
-			if (!fakeResult) {
-				return;
-			}
-
 			fakeResult && setAllProducts(fakeResult);
 		})();
 	}, []);
