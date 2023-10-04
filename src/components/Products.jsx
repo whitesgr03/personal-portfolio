f5d06bae3fa64f951eb893f776778e39b16b1462
Weiss Bai <whitesgr03@gmail.com>
2023-10-04 11:25:21 +0800

    Add img tag properties

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -29,10 +29,15 @@ const Products = forwardRef(
 								<div className="imageWrap">
 									<div className="imageLoading"></div>
 									<img
+										onLoad={onLoad}
 										sizes="(max-width: 1920px) 100vw, 1920px"
-										srcSet={`${product.image.preview.url.phone} 480w, ${product.image.preview.url.table} 1004w, ${product.image.preview.url.laptop} 1372w, ${product.image.preview.url.desktop} 1820w`}
-										src={product.image.preview.url.desktop}
+										srcSet={`${product.image.preview.phone.url} 480w, ${product.image.preview.tablet.url} 1004w, ${product.image.preview.laptop.url} 1372w, ${product.image.preview.desktop.url} 1820w`}
+										src={product.image.preview.desktop.url}
 										alt={product.name}
+										loading={index > 0 ? "lazy" : "eager"}
+										decoding={index > 0 ? "async" : "auto"}
+										width={imageSize?.width}
+										height={imageSize?.height}
 									/>
 								</div>
 							</button>
