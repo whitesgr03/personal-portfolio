e7c976c10a0be857f61ecc6bbd6fa1f65b5b428a
Weiss Bai <whitesgr03@gmail.com>
2023-10-05 15:52:32 +0800

    Import userEvent

--- src/__test__/Header.test.jsx
+++ src/__test__/Header.test.jsx
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
 
+import userEvent from "@testing-library/user-event";
+
 import Header from "../components/Header";
 
 describe("Renders Header Component", () => {
