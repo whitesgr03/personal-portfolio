9a96b5faaec44b37970e220bb163a63165316b59
Weiss Bai <whitesgr03@gmail.com>
2023-10-05 15:37:35 +0800

    Add userEvent tab to test focus

--- src/__test__/Footer.test.jsx
+++ src/__test__/Footer.test.jsx
@@ -6,17 +6,31 @@ import Footer from "../components/Footer";
 
 describe("Renders Footer Component", () => {
 	it("Should press the Tab key to focus an link", async () => {
+		const user = userEvent.setup();
+		const mockShowModal = false;
 
 		render(<Footer showModal={mokeShowModal} />);
 
 		const link = screen.getByRole("link");
-		expect(link).toHaveAttribute("tabIndex", "");
+
+		expect(document.body).toHaveFocus();
+
+		await user.tab();
+
+		expect(link).toHaveFocus();
 	});
 	it("Should disable tab key to focus an link", async () => {
+		const user = userEvent.setup();
+		const mockShowModal = true;
 
 		render(<Footer showModal={mokeShowModal} />);
 
 		const link = screen.getByRole("link");
-		expect(link).toHaveAttribute("tabIndex", "-1");
+
+		expect(document.body).toHaveFocus();
+
+		await user.tab();
+
+		expect(link).not.toHaveFocus();
 	});
 });
