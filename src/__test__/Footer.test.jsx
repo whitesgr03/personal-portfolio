e6940a196c0089c4b22847638d84e31fc502eeba
Weiss Bai <whitesgr03@gmail.com>
2023-10-05 15:33:55 +0800

    Change test name

--- src/__test__/Footer.test.jsx
+++ src/__test__/Footer.test.jsx
@@ -3,16 +3,14 @@ import { render, screen } from "@testing-library/react";
 import Footer from "../components/Footer";
 
 describe("Renders Footer Component", () => {
-	it("Should be able to focus link", () => {
-		const mokeShowModal = false;
+	it("Should press the Tab key to focus an link", async () => {
 
 		render(<Footer showModal={mokeShowModal} />);
 
 		const link = screen.getByRole("link");
 		expect(link).toHaveAttribute("tabIndex", "");
 	});
-	it("Should disable focus link", () => {
-		const mokeShowModal = true;
+	it("Should disable tab key to focus an link", async () => {
 
 		render(<Footer showModal={mokeShowModal} />);
 
