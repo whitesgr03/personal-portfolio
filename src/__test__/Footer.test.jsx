920c68b3056f71244565074d8d22599899816f3f
Weiss Bai <whitesgr03@gmail.com>
2023-10-05 15:34:08 +0800

    Import userEvent

--- src/__test__/Footer.test.jsx
+++ src/__test__/Footer.test.jsx
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
 
+import userEvent from "@testing-library/user-event";
+
 import Footer from "../components/Footer";
 
 describe("Renders Footer Component", () => {
