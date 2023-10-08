cb45c1ccbde3238646581105b98532693580e1f3
Weiss Bai <whitesgr03@gmail.com>
2023-10-08 13:45:19 +0800

    Change loading state default value

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
 	const [product, setProduct] = useState(null);
 	const [showModal, setShowModal] = useState(false);
 	const [imageSize, setImageSize] = useState(null);
-	const [loading, setLoading] = useState(false);
+	const [loading, setLoading] = useState(true);
 
 	const appRef = useRef(null);
 	const aboutRef = useRef(null);
