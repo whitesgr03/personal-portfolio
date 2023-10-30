50ba539a59dd08433885af7cc7763110dd20c384
Weiss Bai <whitesgr03@gmail.com>
2023-10-30 14:33:36 +0800

    Remove import fireEvent

--- src/__test__/About.test.jsx
+++ src/__test__/About.test.jsx
@@ -1,7 +1,5 @@
 import { render, screen } from "@testing-library/react";
 
-import { fireEvent } from "@testing-library/react";
-
 import About from "../components/About";
 
 describe("About Component", () => {
