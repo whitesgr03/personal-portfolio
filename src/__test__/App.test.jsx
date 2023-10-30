94bc4cab9a1804d84cac78eda924bad7e7adce09
Weiss Bai <whitesgr03@gmail.com>
2023-10-30 14:32:28 +0800

    Add mock child component

--- src/__test__/App.test.jsx
+++ src/__test__/App.test.jsx
@@ -2,6 +2,42 @@ import { render, screen } from "@testing-library/react";
 
 import App from "../components/App";
 
+jest.mock("../components/Header", () => ({ darkTheme, onChangeTheme }) => (
+	<div>
+		<button
+			className={darkTheme ? "dark" : "light"}
+			onClick={onChangeTheme}
+			data-testid="changeTheme"
+		>
+			change
+		</button>
+	</div>
+));
+
+jest.mock("../components/Products", () => {
+	const { forwardRef } = jest.requireActual("react");
+	return {
+		__esModule: true,
+		default: forwardRef(({ onShowProduct, showModal, onLoad }, ref) => {
+			return (
+				<div>
+					<button
+						type="button"
+						className={showModal ? "show" : ""}
+						data-testid="showProduct"
+						onClick={() => onShowProduct(null)}
+					>
+						<div>
+							<div data-testid="sibling"></div>
+							<img onLoad={onLoad} alt="" data-testid="image" />
+						</div>
+					</button>
+				</div>
+			);
+		}),
+	};
+});
+
 	it("Should render App component with dark scheme", async () => {
 		window.matchMedia = jest.fn(() => ({
 			matches: true,
